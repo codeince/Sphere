5dd51c5dd2441a16e81141e1b110c3f7504023bc
Code Slasher <ivavoronov@mail.ru>
2024-12-20 23:00:22 +0300

    Unriched

--- src/__main__.py
+++ src/__main__.py
@@ -1,17 +1,16 @@
 from __init__ import Sphere
 from json import loads, dumps
+from os import name, system
 
-lang = 'ru'
+def clear(): return system('cls' if name == 'nt' else 'clear')
+
+lang = 'en'
 lang_build = loads(open(f'src/langs/{lang}.json', 'r', encoding='utf-8').read())
 lang_build = (lang_build if lang_build.get('lang') == 'en' else loads(dumps(lang_build, ensure_ascii=False))).get('text')
 
 answers: dict[dict[str, list[str]]] = loads(open(f'src/langs/answer_files/answers_{lang}.json', 'r', encoding='utf-8').read())
 answers = (answers if answers.get('lang') == 'en' else loads(dumps(answers, ensure_ascii=False))).get('answers')
 
-from rich.console import Console
-
-c = Console()
-c.clear()
 guesser = Sphere(lang_build, answers)
 
 
@@ -24,4 +23,4 @@ while True:
     if question.lower() in lang_build.get('stop_words'):
         exit()
     guesser.ask(question)
-    c.clear()
+    clear()
